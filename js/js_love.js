ed1f1dc800046e7ae4831afbb400d01fd4113f4f
onyoo <roberto9m9@gmail.com>
2016-02-18 14:32:22 +0100

    Done.

--- js/js_love.js
+++ js/js_love.js
@@ -1,5 +1,14 @@
 // Functions here
 function showJsLove() {
+
+  console.log("I")
+  $.getJSON('https://api.github.com/repos/rails/rails/commits', function(response){
+    console.log("Javascript");
+  });
+  // .fail(function(error){
+  //   console.log("Ruby");
+  // });
+  console.log("Love")
 };
 
 $(document).ready(function (){
