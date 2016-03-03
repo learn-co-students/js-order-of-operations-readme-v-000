f4cadf806c33d780529ffb2392c9393dd6dd3b54
Shana L Moore <shana.lavina.moore@gmail.com>
2016-03-02 19:45:09 -0800

    Done.

--- js/js_love.js
+++ js/js_love.js
@@ -1,7 +1,14 @@
 // Functions here
 function showJsLove() {
+  $.getJSON('https://api.github.com/repos/rails/rails/commits', function  (response) {
+    console.log("JavaScript")
+  })
 };
 
 $(document).ready(function (){
   showJsLove();
+
+  console.log("I")
+  showJsLove();
+  console.log("Love")
 });
