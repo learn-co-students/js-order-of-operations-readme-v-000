ba7bff76a64c2f49183329946760b91f4637bfb0
Chen Yang <yangc5@mail.uc.edu>
2016-03-07 22:26:34 +0000

    Done.

--- js/js_love.js
+++ js/js_love.js
@@ -1,5 +1,10 @@
 // Functions here
 function showJsLove() {
+  console.log("I");
+  $.get('https://api.github.com/repos/rails/rails/commits', function(){
+    console.log("JavaScript");
+  });
+  console.log("Love");
 };
 
 $(document).ready(function (){
