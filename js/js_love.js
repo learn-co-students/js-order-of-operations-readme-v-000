dcbfb9f5245676f43301d898e37a29eeeaa17849
empireofryan <rjohnson2001@gmail.com>
2016-03-27 02:45:27 -0400

    Done.

--- js/js_love.js
+++ js/js_love.js
@@ -1,5 +1,11 @@
 // Functions here
 function showJsLove() {
+  console.log("I");
+    $.getJSON("https://api.github.com/repos/rails/rails/commits", function(response){
+      // This is our success callback which is called automatically
+      console.log("Love");
+    });
+    console.log("JavaScript");
 };
 
 $(document).ready(function (){
