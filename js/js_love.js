6c4bf8b28174a1bcc1017ba2b0c59ca2ed73dfb5
NStephenson <nickstephenson90@gmail.com>
2016-04-08 15:41:50 -0400

    made all tests pass

--- js/js_love.js
+++ js/js_love.js
@@ -1,5 +1,11 @@
 // Functions here
 function showJsLove() {
+  $.getJSON('https://api.github.com/repos/rails/rails/commits', function(response){
+    console.log('Javascript')
+  });
+  console.log('I');
+  console.log('Love');
+
 };
 
 $(document).ready(function (){
