7674cc26b069d947cac0a8650dcf6ed9982dede9
Chris Aporta <christopher.aporta@gmail.com>
2016-04-10 19:04:18 -0400

    complete lab

--- js/js_love.js
+++ js/js_love.js
@@ -1,7 +1,11 @@
-// Functions here
 function showJsLove() {
-};
+  $.getJSON('https://api.github.com/repos/rails/rails/commits', function (resp) {
+    console.log("JavaScript");
+  });
+}
 
 $(document).ready(function (){
+  console.log("I");
   showJsLove();
+  console.log("Love");
 });
