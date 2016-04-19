8ebc6ed553a1febc349aa61f58ccea72951a1c8d
jjlippman <jjlippman@earthlink.net>
2016-04-19 12:38:51 -0600

    Done.

--- js/js_love.js
+++ js/js_love.js
@@ -1,7 +1,14 @@
 // Functions here
 function showJsLove() {
+  $.get('https://api.github.com/repos/rails/rails/commits' function(data){
+
+      $('body').append(data);
+
+  });
+
 };
 
 $(document).ready(function (){
   showJsLove();
+  alert('here');
 });
