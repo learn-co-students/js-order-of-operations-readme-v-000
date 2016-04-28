e803a8bb4d22f74c39394dc33c938e787b91c160
Louiefigz <figueroa.aaron@gmail.com>
2016-04-28 00:45:32 -0400

    Done.

--- js/js_love.js
+++ js/js_love.js
@@ -1,5 +1,13 @@
 // Functions here
+
+var url='https://api.github.com/repos/rails/rails/commits'
 function showJsLove() {
+  console.log('I');
+  $.getJSON(url, function(){
+    console.log('JavaScript');
+  })
+    console.log('Love');
+
 };
 
 $(document).ready(function (){
