4941f6ce3c08119ab5edf7471c7460778ec041c1
agdavid <antonio.g.david@gmail.com>
2016-05-24 13:40:22 -0400

    relocate console.log(Love) to fully showcase order of operations

--- js/js_love.js
+++ js/js_love.js
@@ -3,9 +3,9 @@ function showJsLove() {
   console.log("I");
   var url = 'https://api.github.com/repos/rails/rails/commits';
   $.getJSON(url, function(response) {
-    console.log("Love");
     console.log("JavaScript");
   }); //.get .fail
+  console.log("Love");
 }; //showJsLove
 
 $(document).ready(function (){
