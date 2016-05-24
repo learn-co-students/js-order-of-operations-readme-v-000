9163f0999286808afb842c377f573ba71bcb47eb
agdavid <antonio.g.david@gmail.com>
2016-05-24 13:39:28 -0400

    removed error callback to satisfy tests

--- js/js_love.js
+++ js/js_love.js
@@ -1,6 +1,12 @@
 // Functions here
 function showJsLove() {
-};
+  console.log("I");
+  var url = 'https://api.github.com/repos/rails/rails/commits';
+  $.getJSON(url, function(response) {
+    console.log("Love");
+    console.log("JavaScript");
+  }); //.get .fail
+}; //showJsLove
 
 $(document).ready(function (){
   showJsLove();
