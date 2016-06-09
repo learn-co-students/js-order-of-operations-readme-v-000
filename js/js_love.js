81acd676b81702ab1a17b17e81b2a8c7d321fe55
jasonpaulso <hello@jasonpaulsouthwell.com>
2016-06-09 12:18:29 -0700

    Done.

--- js/js_love.js
+++ js/js_love.js
@@ -1,5 +1,11 @@
 // Functions here
 function showJsLove() {
+  console.log('I');
+  var url = "https://api.github.com/repos/rails/rails/commits"
+  $.getJSON(url, function() {
+    console.log('JavaScript')
+  });
+  console.log('Love');
 };
 
 $(document).ready(function (){
