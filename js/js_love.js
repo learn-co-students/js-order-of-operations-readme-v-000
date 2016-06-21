306d19eb3110d01551ee59c0fd51ca718c77bfe6
Christopher Bruen <bruen.chris@gmail.com>
2016-06-21 00:36:39 +0000

    Done.

--- js/js_love.js
+++ js/js_love.js
@@ -1,7 +1,13 @@
 // Functions here
 function showJsLove() {
+  $.getJSON("https://api.github.com/repos/rails/rails/commits", function(data) {
+    console.log("Javascript");
+  })
+
 };
 
 $(document).ready(function (){
+  console.log("I");
   showJsLove();
+  console.log("Love");
 });
