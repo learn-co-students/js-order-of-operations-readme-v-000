17f073c12de4e15848b375492d544e74e20d6847
James Novak <jamesnovak90@gmail.com>
2016-07-03 16:46:18 -0400

    Done.

--- js/js_love.js
+++ js/js_love.js
@@ -1,5 +1,10 @@
 // Functions here
 function showJsLove() {
+  $.getJSON("https://api.github.com/repos/rails/rails/commits", function(response){
+    console.log("Love");
+    console.log("Javascript");
+  });
+  console.log("I");
 };
 
 $(document).ready(function (){
