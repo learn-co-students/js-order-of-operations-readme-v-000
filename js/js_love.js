986404768875c8daf901eda333ac27ed589d175b
thebrianemory <the@brianemory.com>
2016-07-09 11:49:44 -0700

    Done.

--- js/js_love.js
+++ js/js_love.js
@@ -1,5 +1,12 @@
 // Functions here
 function showJsLove() {
+  var url = 'https://api.github.com/repos/rails/rails/commits';
+  console.log("I");
+  $.getJSON(url, function() {
+    console.log("Success");
+  })
+  console.log("Love");
+  console.log("JavaScript");
 };
 
 $(document).ready(function (){
