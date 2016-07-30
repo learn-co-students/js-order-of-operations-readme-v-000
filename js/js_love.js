66891a5306ceac76b61d41581afade198c527f0d
Jason Casares <jasonc@csdrafting.com>
2016-07-30 17:50:23 +0000

    Done.

--- js/js_love.js
+++ js/js_love.js
@@ -1,5 +1,8 @@
 // Functions here
 function showJsLove() {
+  $.getJSON("https://api.github.com/repos/rails/rails/commit", function(response) {
+    console.log(Love);
+  })
 };
 
 $(document).ready(function (){
