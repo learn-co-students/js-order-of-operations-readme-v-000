bc48d6ec0e20934cc445bd0295cbac203196cd7f
nscarlyon <natasha.carlyon@gmail.com>
2016-08-09 11:38:35 -0400

    Done.

--- js/js_love.js
+++ js/js_love.js
@@ -1,7 +1,15 @@
 // Functions here
+
+
 function showJsLove() {
+  $.getJSON("https://api.github.com/repos/rails/rails/commits", function(response) {
+    console.log("success");
+  })
 };
 
 $(document).ready(function (){
+  console.log("I")
+  console.log("Love")
+  console.log("JavaScript")
   showJsLove();
 });
