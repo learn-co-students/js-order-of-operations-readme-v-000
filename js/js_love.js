3950f8fac26fbfbd9b5603d1b9010c9e48db857f
Jason <jasonkwong11@gmail.com>
2016-08-26 20:48:41 -0400

    done

--- js/js_love.js
+++ js/js_love.js
@@ -1,7 +1,12 @@
 // Functions here
 function showJsLove() {
+  console.log("Love");
+  $.getJSON('https://api.github.com/repos/rails/rails/commits', function(response){
+    console.log("JavaScript");
+  })
 };
 
 $(document).ready(function (){
+  console.log("I");
   showJsLove();
 });
