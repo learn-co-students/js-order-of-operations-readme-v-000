cf4330a028ba1048aaf587b8898170eb03429d11
Joseph Lim <joseph.d.lim@gmail.com>
2016-09-21 10:26:52 -0400

    Done.

--- js/js_love.js
+++ js/js_love.js
@@ -1,6 +1,18 @@
 // Functions here
 function showJsLove() {
+  console.log('I');
+  $.getJSON('https://api.github.com/repos/rails/rails/commits', function(d) {
+    console.log('javascript');
+  });
+  console.log('love');
 };
+
+var jqxhr = $.get( "example.php", function() {
+  alert( "success" );
+})
+  .done(function() {
+    alert( "second success" );
+  })
 
 $(document).ready(function (){
   showJsLove();
