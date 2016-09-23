39070a66792569a1274b14c991758949355f254e
peterberwind <peterberwind@gmail.com>
2016-09-23 14:53:35 -0400

    Done.

--- js/js_love.js
+++ js/js_love.js
@@ -1,5 +1,11 @@
 // Functions here
 function showJsLove() {
+  console.log("I");
+  var endpoint = "https://api.github.com/repos/rails/rails/commits"
+  $.getJSON(endpoint, function(response) {
+    console.log("Javascript");
+  });
+  console.log("Love");
 };
 
 $(document).ready(function (){
