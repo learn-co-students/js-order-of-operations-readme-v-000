0483b7b4e492f17524a8141d1b45844ae1a7f379
jd2rogers2 <jd2rogers2@gmail.com>
2016-09-29 14:51:07 -0400

    donzo

--- js/js_love.js
+++ js/js_love.js
@@ -1,5 +1,11 @@
 // Functions here
 function showJsLove() {
+  const url = "https://api.github.com/repos/rails/rails/commits";
+  console.log("I");
+  $.getJSON(url, function() {
+    console.log("JavaScript");
+  });
+  console.log("Love");
 };
 
 $(document).ready(function (){
