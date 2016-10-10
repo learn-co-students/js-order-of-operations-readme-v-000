c11a845dec82da48939e3abb74e8a47eeb0d6b82
kaileighrose <kaileighrose@gmail.com>
2016-10-10 15:53:42 -0400

    Done.

--- js/js_love.js
+++ js/js_love.js
@@ -1,5 +1,12 @@
+("use strict");
 // Functions here
 function showJsLove() {
+  $.getJSON("https://api.github.com/repos/rails/rails/commits", function(response){
+  console.log("I");
+  console.log("Love");
+  console.log("JavaScript");
+  return response;
+})
 };
 
 $(document).ready(function (){
