0dc6ff12fd486ea6e270046ae987cce4f1c98203
rgylling <mikegylling@yahoo.com>
2016-10-10 10:31:53 -0700

    Done.

--- js/js_love.js
+++ js/js_love.js
@@ -1,6 +1,10 @@
 // Functions here
 function showJsLove() {
-};
+  console.log("I");
+  $.getJSON("https://api.github.com/repos/rails/rails/commits")
+    console.log("Javascript");
+  };
+  console.log("Love");
 
 $(document).ready(function (){
   showJsLove();
