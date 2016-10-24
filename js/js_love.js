ce50637360940a09e45dc5071c0c9c55ab483841
Christian Whitesides <whitesidescr@gmail.com>
2016-10-24 17:35:27 -0600

    Done.

--- js/js_love.js
+++ js/js_love.js
@@ -1,10 +1,10 @@
 // Functions here
 function showJsLove() {
-  console.log("I")
-  console.log("Love")
+  console.log("I");
   $.getJSON('https://api.github.com/repos/rails/rails/commits', function(response) {
-    console.log("Javascript")
-  })
+    console.log("Javascript");
+  });
+  console.log("Love");
 };
 
 $(document).ready(function (){
