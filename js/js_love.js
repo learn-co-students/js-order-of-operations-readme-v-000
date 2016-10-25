5a0d544eb762692d12938147f894df4f664d08cd
SiamKing <thesiameseelectric@gmail.com>
2016-10-25 11:41:35 -0700

    I <3 JS"
    exit

--- js/js_love.js
+++ js/js_love.js
@@ -1,6 +1,11 @@
 // Functions here
 function showJsLove() {
-};
+  $.getJSON("https://api.github.com/repos/rails/rails/commits", function(response) {
+    console.log("I");
+    console.log("Love");
+    console.log("Javascript");
+  });
+}
 
 $(document).ready(function (){
   showJsLove();
