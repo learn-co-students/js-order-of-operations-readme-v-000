a0edf3676d3cfb1c8d3aa30e1b90cb71f412d738
tpstar <hanlee12@gmail.com>
2016-10-28 20:17:48 -0500

    done

--- js/js_love.js
+++ js/js_love.js
@@ -1,7 +1,17 @@
 // Functions here
 function showJsLove() {
+  console.log("I");
+  $.getJSON("https://api.github.com/repos/rails/rails/commits", function(response){
+
+    console.log("Love");
+    // This is our success callback which is called automatically
+  });
+  console.log("Javascript");
 };
 
 $(document).ready(function (){
+
   showJsLove();
+
+
 });
