42e4f759452d1291c43a0c82a83ca678cd25cd15
lenn hypolite <lennhydesign@gmail.com>
2016-11-13 17:52:43 -0500

    Done

--- js/js_love.js
+++ js/js_love.js
@@ -5,3 +5,12 @@ function showJsLove() {
 $(document).ready(function (){
   showJsLove();
 });
+
+console.log("I");
+
+function showJsLove() {
+  console.log("Javascript");
+  $.getJSON("https://api.github.com/repos/rails/rails/commits", function(response){
+  });
+}
+console.log("love");
