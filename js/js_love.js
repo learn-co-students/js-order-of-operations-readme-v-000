6c7232369bbb9f18126142c0c2337b0ab697400c
Brianna Titone <briannactitone@gmail.com>
2016-12-28 03:53:31 +0000

    Done.

--- js/js_love.js
+++ js/js_love.js
@@ -1,7 +1,14 @@
 // Functions here
 function showJsLove() {
+  $.getJSON("https://api.github.com/repos/rails/rails/commits", function(response){
+  // This is our success callback which is called automatically
+    console.log("JavaScript");
+  })
+  
 };
 
 $(document).ready(function (){
+  console.log("I");
   showJsLove();
+  console.log("Love")
 });
