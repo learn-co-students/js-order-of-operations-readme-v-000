09165803a4f17e74af623899ac27cbeff91df1d9
Laurent <lmilne360@gmail.com>
2017-01-11 18:25:24 -0500

    Done.

--- js/js_love.js
+++ js/js_love.js
@@ -1,7 +1,14 @@
 // Functions here
 function showJsLove() {
+	console.log("I")
+	$.getJSON("https://api.github.com/repos/rails/rails/commits", function(response){
+		console.log("Javascript")
+	});
+	console.log("Love")
 };
 
 $(document).ready(function (){
+
   showJsLove();
+
 });
