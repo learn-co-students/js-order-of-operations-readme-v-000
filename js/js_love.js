75643093282944d00764ae118678618b144e3a82
bmolina-nyc <bruce.molina.81@gmail.com>
2017-01-23 22:33:56 -0500

    Done.

--- js/js_love.js
+++ js/js_love.js
@@ -1,7 +1,17 @@
 // Functions here
 function showJsLove() {
+  console.log("I");
+$.getJSON("https://api.github.com/repos/rails/rails/commits", function(response){
+  console.log("Javascript");
+});
+console.log("Love");
 };
 
 $(document).ready(function (){
   showJsLove();
 });
+
+
+// https://api.github.com/repos/rails/rails/commits
+
+
